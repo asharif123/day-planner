ac06895aca8af733efb2e8467f8ea2f92b121279
Awad Sharif <awadsharif9@gmail.com>
2021-10-18 23:59:24 -0700

    added time blocks to index.html webpage

--- assets/scripts.js
+++ assets/scripts.js
@@ -27,7 +27,7 @@ for (var i = 0; i < timeBlocks.length; i++) {
 
 // function that saves events stored by user
 // user clicks on a event and has the option to add information
-// once info is entered, user can save that event
+// once info is entered, user can save that event from add button
 // user can edit that event
 function saveEvent(event) {
 
