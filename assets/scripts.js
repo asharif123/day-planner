566a008ac786c6a9c11be3b35257eca7bd0db089
Awad Sharif <awadsharif9@gmail.com>
2021-10-18 20:24:49 -0700

    added timeblocks to index.html

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,14 @@
 // array of time blocks from 9 am to 6 pm (ex: 9 - 9 am, 13 - 1 pm, 17 - 5pm, 18 - 6 pm)
 var timeBlocks = [9,10,11,12,13,14,15,16,17,18]
-var timings = [`<p>9 AM</p>`, `<p>10 AM<\p>`, `<p>11 AM</p>`, `<p>12 PM</p>`, `<p>1 PM</p>`, `<p>2 PM</p>`, `<p>3 PM</p>`,`<p>4 PM</p>`, `<p>4 PM</p>`, `<p>5 PM</p>`];
+var timings = [`<p class="timeBlocks-event-time mx-1">9 AM</p>`, `<p class="timeBlocks-event-time">10 AM<\p>`, `<p class="timeBlocks-event-time">11 AM</p>`, `<p class="timeBlocks-event-time">12 PM</p>`, `<p class="timeBlocks-event-time mx-1">1 PM</p>`, `<p class="timeBlocks-event-time mx-1">2 PM</p>`, `<p class="timeBlocks-event-time mx-1">3 PM</p>`,`<p class="timeBlocks-event-time mx-1">4 PM</p>`, `<p class="timeBlocks-event-time mx-1">5 PM</p>`, `<p class="timeBlocks-event-time mx-1">6 PM</p>`];
 // display current date and time
 var currentDay = $("#currentDay");
-currentDay.text(moment().format("MMM Do YY, h:mm:ss:a"));
+
+function displayTime() {
+    currentDay.text(moment().format('MMM DD, YYYY [at] hh:mm:ss a'));
+}
+
+setInterval(displayTime(), 1000)
 
 // using moment.js to determine if time in array is past, present or future
 // function determines if timearray is in past, present, or future
@@ -17,8 +22,7 @@ function pastPresentFuture(hour) {
 // add each timeblock to the timeblocks container
 var eventBlocks = $("#timeblocks");
 for (var i = 0; i < timeBlocks.length; i++) {
-    console.log(i);
-    eventBlocks.append(` ${timings[i]}<textarea class="${pastPresentFuture(timeBlocks[i])}"></textarea>`);
+    eventBlocks.append(`<div class="timeblock-information d-flex my-4">${timings[i]}<textarea class=" mx-3 ${pastPresentFuture(timeBlocks[i])}"></textarea></div>`);
 }
 
 // function that saves events stored by user
