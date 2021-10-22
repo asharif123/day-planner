deefa73e806d157d0c4eac647dba4225440f7496
Awad Sharif <awadsharif9@gmail.com>
2021-10-21 18:11:21 -0700

    added loadEventData() function to load data onto each particular textbox

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,7 +3,8 @@ var timeBlocks = [9,10,11,12,13,14,15,16,17,18]
 var timings = [`<p class="timeBlocks-event-time mx-1">9:00</p>`, `<p class="timeBlocks-event-time">10:00</p>`, `<p class="timeBlocks-event-time">11:00</p>`, `<p class="timeBlocks-event-time">12:00</p>`, `<p class="timeBlocks-event-time mx-1">1:00</p>`, `<p class="timeBlocks-event-time mx-1">2:00</p>`, `<p class="timeBlocks-event-time mx-1">3:00</p>`,`<p class="timeBlocks-event-time mx-1">4:00</p>`, `<p class="timeBlocks-event-time mx-1">5:00</p>`, `<p class="timeBlocks-event-time mx-1">6:00</p>`];
 // display current date and time
 var currentDay = $("#currentDay");
-// save button
+
+
 function displayTime() {
     currentDay.text(moment().format('MMM DD, YYYY [at] hh:mm:ss a'));
 }
@@ -22,44 +23,59 @@ function pastPresentFuture(hour) {
 // add each timeblock to the timeblocks container
 var eventBlocks = $("#timeblocks");
 for (var i = 0; i < timeBlocks.length; i++) {
-    eventBlocks.append(`<div class="timeblock-information d-flex my-4">${timings[i]}<form><textarea data-index=${i} class="timeblock-event-information mx-3 ${pastPresentFuture(timeBlocks[i])}"></textarea><button type="button" class="btn btn-primary" onclick="saveEvent(event)" id="saveButton">Save</button></form></div>`);
+    eventBlocks.append(`<div class="timeblock-information d-flex my-4">${timings[i]}<form><textarea id=${i} data-index=${i} class="timeblock-event-information mx-3 ${pastPresentFuture(timeBlocks[i])}"></textarea><button type="button" class="btn btn-primary" id="saveButton" onclick="saveEvent(event)">Save</button></form></div>`);
 }
 
 // function that saves events stored by user
-// user clicks on a event and has the option to add information
-// once info is entered, user can save that event from add button
-// user can edit that event
-// when user clicks save button, find the save button's closest textbox information
-// save in localstorage
-var saveButton = $("#saveButton");
-var allEventInfo = [];
+// retrieve information from local storage
+// if you dont have any info event storage in local storage, defaults to square bracket
+// this preserves all event info as array
+var allEventInfo = JSON.parse(localStorage.getItem("eventStorage")) || [];
 
+// loadEventData loads the text information in each text box with its corresponding unique id
+function loadEventData() {
+    for (var i = 0; i < allEventInfo.length; i++) {
+        $("#" + allEventInfo[i].id).val(allEventInfo[i].eventInformation);
+    }
+}
+
+loadEventData();
+
+// save event called when user clicks on savebutton
 function saveEvent(event) {
-    event.preventDefault();
+    event.preventDefault;
     var element = (event.target);
     if (element.matches("#saveButton")) {
+        // user cannot submit empty info when clicking on save button
+        if ($(element).siblings("textarea").val() === '') {
+            return
+        }
+
+        
         var eventText = $(element).siblings("textarea").val().trim();
         // allEventInfo.push(eventText);
         var eventStorage = {
-            eventInformation: eventText
+            eventInformation: eventText,
+            id: $(element).siblings("textarea").attr("id")
         }
-
+        allEventInfo.push(eventStorage);
         // sees every event stored in the list
         console.log(eventStorage.eventInformation);
         
         // id for each textbox
         console.log($(element).siblings("textarea").attr("data-index"));
 
-        localStorage.setItem("eventStorage", JSON.stringify(eventStorage));
+        localStorage.setItem("eventStorage", JSON.stringify(allEventInfo));
+        // render
         var lastPlayerInfo = JSON.parse(localStorage.getItem("eventStorage"));
-        $(element).siblings("textarea").text(lastPlayerInfo.eventInformation);
-
+        ($(element).siblings("textarea").text(lastPlayerInfo.eventInformation));
 
     }
-}
+
+};
+  
 
 
 
-saveButton.on("click", saveEvent);
 
 
